110301a8d7a8c9ccfbb4bf14c974130e08196e8f
kuyeol <zimop12@gmail.com>
2023-12-10 06:45:46 +0000

    a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("He fsddddWorld!");
+        System.out.println("He fsdfffdddWorld!");
     }
 }
