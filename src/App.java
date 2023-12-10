a8178d15429928a9ebb73f9e4ea6587e6249d68e
kuyeol <125532184+kuyeol@users.noreply.github.com>
2023-12-10 16:10:00 +0900

    App.java 업데이트

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
+Scanner scanner = new Scanner(System.in);
+
         System.out.println("H1dddd11e1!");
     }
 }
