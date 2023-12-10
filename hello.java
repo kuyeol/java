51124e60d247d7d2f16192a90c882209018584ef
kuyeol <125532184+kuyeol@users.noreply.github.com>
2023-12-09 21:53:26 -0500

    hello.java 업데이트

--- hello.java
+++ hello.java
@@ -1,6 +1,10 @@
-public class hello {
- public static void main(String[] args) {
-    System. out.println("s12" );
- }
-}
 
+
+import java.util.Scanner;
+
+class hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // start coding here
+    }
+}
