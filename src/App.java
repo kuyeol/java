2a1b61fa69c944b2a74865d969ecbb0730bd2232
kuyeol <125532184+kuyeol@users.noreply.github.com>
2023-12-10 15:46:01 +0900

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("He fsdfffdddWorld!");
+        System.out.println("He forld!");
     }
 }
