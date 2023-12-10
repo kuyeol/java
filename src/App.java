40cd4744d4e3dfaa848b51c8779c1423c9233377
kuyeol <125532184+kuyeol@users.noreply.github.com>
2023-12-10 16:17:28 +0900

    App.java 업데이트

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
 Scanner scanner = new Scanner(System.in);
 int sum = 0;
 while (scanner.hasNextInt()) {
