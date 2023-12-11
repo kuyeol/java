a37e0ff6619fc24ab3bd601a45a2a9bd481e99fb
kuyeol <125532184+kuyeol@users.noreply.github.com>
2023-12-10 19:02:48 -0500

    App.java 업데이트

--- src/App.java
+++ src/App.java
@@ -1,15 +1,33 @@
+//import java.util.Scanner;
+//
+//public class App {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int sum = 0;
+//        while (true) {
+//            int num = scanner.nextInt();
+//            if (num == 0) {
+//                break;
+//            }
+//            sum += num;
+//        }
+//        System.out.println("The sum of the sequence is: " + sum);
+//    }
+//}
+
 import java.util.Scanner;
 
-
 public class App {
-    public static void main(String[] args)  {
-Scanner scanner = new Scanner(System.in);
-int sum = 0;
-while (scanner.hasNextInt()) {
-    int elem = scanner.nextInt();
-    sum += elem;
-
-        
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int num;
+        do {
+            num = scanner.nextInt();
+            if (num != 0) {
+                sum += num;
+            }
+        } while (num != 0);
+        System.out.println("The sum of the sequence is: " + sum);
     }
-        System.out.println(sum);
 }
